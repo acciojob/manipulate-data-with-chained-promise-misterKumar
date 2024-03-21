a5ee842d0ff4796eea955e02c5b749bbf7e7884c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-21 08:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+let arrayOfNumber  = [1,2,3,4];
+function solution(arrayOfNumber){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			const evenNumber = arrayOfNumber.filter((num) => num%2==0);
+			output.innerText = evenNumber.join(",");
+			resolve(evenNumber);
+		},1000);
+	})
+	.then((evenNumber) => {
+		return new Promise((resolve,reject) => {
+			setTimeout(()=>{
+				const EvenMulti = evenNumber.map((num) => num*2);
+				output.innerText = EvenMulti.join(",");
+				resolve(EvenMulti);
+			},2000);
+		});
+	})
+	.catch((error) =>{
+		console.log("Error", error);
+	});
+
+	
+	
+}
+solution(arrayOfNumber);
